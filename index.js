56962fc744693aeb0a505c5cafc8b72889e3abfe
Nika Janjghava <nikusharoot@gmail.com>
2023-07-11 12:50:51 +0400

    კოდის განახლება

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('HT Solutions - Demo APP Presentation'));
+app.get('/', (req, res) => res.send('<h1> HT Solutions - Demo APP Presentation </h1> '));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
